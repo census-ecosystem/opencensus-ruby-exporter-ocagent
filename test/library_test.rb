e3010e127ae0c374a7f46d7c80218924d2982b7c
Jiren Patel <jirenpatel@gmail.com>
2019-03-29 21:14:20 +0530

    Added integration test

--- test/library_test.rb
+++ test/library_test.rb
@@ -21,4 +21,41 @@ describe OpenCensus::OCAgent do
   it "has a version number" do
     refute_nil OpenCensus::OCAgent::VERSION
   end
+
+  it "export spans to agent service" do
+    skip unless ENV["AGENT_SERVICE_ADDRESS"]
+
+    exporter = OpenCensus::Trace::Exporters::OCAgent.new(
+      service_name: "RubyOCAgent-Test",
+      agent_service_address: ENV["AGENT_SERVICE_ADDRESS"]
+    )
+    OpenCensus::Trace.configure do |config|
+      config.exporter = exporter
+    end
+
+    OpenCensus::Trace.start_request_trace do |root_context|
+      OpenCensus::Trace.in_span("span1") do |span1|
+        span1.put_attribute :data, "Outer span"
+        sleep 0.1
+        OpenCensus::Trace.in_span("span2") do |span2|
+          span2.put_attribute :data, "Inner span"
+          sleep 0.2
+        end
+        OpenCensus::Trace.in_span("span3") do |span3|
+          span3.put_attribute :data, "Another inner span"
+          sleep 0.1
+        end
+      end
+      exporter.export root_context.build_contained_spans
+    end
+    OpenCensus::Trace.start_request_trace do |root_context|
+      OpenCensus::Trace.in_span("span4") do |span4|
+        span4.put_attribute :data, "Fast span"
+      end
+      exporter.export root_context.build_contained_spans
+    end
+
+    exporter.stop
+    sleep 1
+  end
 end
